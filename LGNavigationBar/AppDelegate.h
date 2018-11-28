7c5dbe1638fd501503b2530a97ce4b261cd176d4
Arrcen-LG <515930926@qq.com>
2018-11-28 12:31:35 +0800

    Initial Commit

---
+++ LGNavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGNavigationBar
+//
+//  Created by Arrcen-LG on 2018/11/28.
+//  Copyright © 2018 Arrcen-LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LGNavigationBar/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LGNavigationBar
+//
+//  Created by Arrcen-LG on 2018/11/28.
+//  Copyright © 2018 Arrcen-LG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
